53e38bd62b03c078c6dded93562710384501c271
recsnik.laszlo <recsnik.laszlo@BOT212-006>
2024-02-12 12:28:17 +0100

    Datastructure created and file read

--- Hianyzas_RL_0212/Program.cs
+++ Hianyzas_RL_0212/Program.cs
@@ -1,8 +1,30 @@
-﻿
+﻿using System.Runtime.CompilerServices;
+
+string[] adat = File.ReadAllLines("szeptember.csv");
+List<Hiany> data = new List<Hiany>();
 
 
 
 
+struct Hiany
+{
+    public string Nev;
+    public string Oszt;
+    public string elso;
+    public string utolso;
+    public int orakszama;
 
 
+    public Hiany(string line)
+    {
+        //var -> Variable, ha nem tudod milyen típusú adatot kapsz, ő kiértékeli
+        string[] splitted = line.Split(';');
+        this.Nev = splitted[0];
+        this.Oszt = splitted[1];
+        this.elso = splitted[2];
+        this.utolso = splitted[3];
+        this.orakszama = int.Parse(splitted[4]);
 
+    }
+
+}
